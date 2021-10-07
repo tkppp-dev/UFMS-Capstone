c0de51fb7fdec298de6ffa9b4dcfe494ee4a517c
root <taekg.park@gmail.com>
2021-10-08 02:36:12 +0900

    Navigation and UI of Usage Search(Home Scree)

--- app/README.md
+++ app/README.md
@@ -3,3 +3,6 @@ https://f-dever-error-log.tistory.com/17
 
 ###### 네비게이션 헤더
 https://dlee0129.tistory.com/15
+
+###### Date Picker
+https://github.com/henninghall/react-native-date-picker
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,25 +1,42 @@
-import React from "react";
-import { Button } from "react-native";
-import styled from "styled-components";
-import Header from '../src/components/Header'
+import React from 'react';
+import { Button, ScrollView } from 'react-native';
+import styled from 'styled-components';
+import Header from '../src/components/Header';
+import Building from '../src/components/Building';
 
 const Container = styled.SafeAreaView`
   flex: 1;
   align-items: center;
-`
+`;
 
 const StyledText = styled.Text`
   font-size: 30px;
   margin-bottom: 10px;
-`
+`;
 
-const Home = function({navigation}){
-  return(
+const Home = function ({ navigation }) {
+  const builings = [
+    { id: 1, name: '대양 AI 센터' },
+    { id: 2, name: '광개토관' },
+    { id: 3, name: '율곡관' },
+    { id: 4, name: '영실관' },
+    { id: 5, name: '우정당' },
+    { id: 6, name: '대양홀' },
+  ];
+
+  return (
     <Container>
       <Header />
-      <Button title="Go to Sign In Screen" onPress={() => navigation.navigate('HomeSignIn')}/>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={{ width: '100%', height: '100%' }}
+      >
+        {builings.map((item) => (
+          <Building key={item.id} buildingData={item} navigation={navigation} />
+        ))}
+      </ScrollView>
     </Container>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
