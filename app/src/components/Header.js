067337c517ba70c96a4d18b873db7dadcbc71aa1
YunJehyeok <dbswpgur2@naver.com>
2021-10-08 11:59:06 +0900

    10/8

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -2,8 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 //import PropTypes from 'prop-types';
 import IconButton from './IconButton';
-import { images } from '../images'
-
+import { images } from '../images';
 
 const Container = styled.View`
   flex-direction: row;
@@ -12,20 +11,20 @@ const Container = styled.View`
   width: 100%;
   height: 50px;
   background-color: white;
-`
+`;
 
 const LogoText = styled.Text`
   margin-left: 10px;
   font-size: 30px;
-`
+`;
 
-const Header = function(){
-  return(
+const Header = function () {
+  return (
     <Container>
       <LogoText>LOGO</LogoText>
       <IconButton type={images.menu_black} />
     </Container>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
